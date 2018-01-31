d6dafde4a6fb5b3b7006f5b16f48875c5b7b01c6
Gabriel Jay Huerte <gjhuerte@gmail.com>
2018-02-01 03:21:25 +0800

    Added Some Commands

--- first.php
+++ first.php
@@ -1,6 +1,17 @@
 <?php 
 
 	echo "hello world";
+	echo "magdadagdag ako";
+	echo "Maraming parts ulit";
+	echo "maraming commands";
+	echo "Okay sya"
+
+	if(true)
+	{
+		return 'wala';
+	}
+	
+	echo "Okay sya"
 
 ?>
 
